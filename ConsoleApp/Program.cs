2b9d256f77d3805d929a37f820e36e9a21da3bdc
dmizinchenk <dmizinchenk@rambler.ru>
2024-04-20 13:17:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string value = "9459914441654073";
+            int size = value.Length;
+            //object locker = new object();
+            List<string> palindromes = new List<string>();
+
+            ParallelLoopResult res = Parallel.For(2, size, (iter) =>
+            {
+                for (int i = 0; i <= size - iter; i++)
+                {
+                    string s = value.Substring(i, iter);
+                    bool isPalindrome = true;
+                    for (int j = 0; j < s.Length / 2; j++)
+                    {
+                        if (s[j] != s[s.Length - 1 - j])
+                        {
+                            isPalindrome = false;
+                            break;
+                        }
+                    }
+                    if (isPalindrome)
+                    {
+                        //lock (locker)
+                        {
+                            palindromes.Add(s);
+                        }
+                    }
+                }
+            });
+            Console.WriteLine(res.IsCompleted);
+
+            foreach (string s in palindromes)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
